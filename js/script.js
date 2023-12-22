9ad1be57eeca7a803c5b783de92ed1055fca4fc9
nana <goldbarrr5@gmail.com>
2023-12-22 10:52:27 +0900

    loading 스크립트로 넣기

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(function () {
   // 대상을 변수로 저장
   const $window = $(window);
-  const $loading = $('.loading');
+  const $body = $('body');
+  const $loading = `<div class="loading">
+  <div class="lds-heart"><div></div></div>
+</div>`;
+  // body에 맨끝(마지막) 부분에 집어넣기
+  $body.append($loading);
+  const $target = $('.loading');
 
   // 로딩이 완료되면
   $window.on('load', function () {
     // 너무 빨리 사라지므로 조금있다가 사라지게끔
     // setTimeout(동작. 시간)
     setTimeout(function () {
-      $loading.fadeOut();
+      $target.fadeOut();
     }, 1000);
   });
 });
